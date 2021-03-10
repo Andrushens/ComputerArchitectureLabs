55ea5c293d1174a4a384b4134a0cb3c0f1fb5909
andrushens <thelasmo3228@gmail.com>
2021-03-10 09:33:22 +0300

    final changes

--- lab01/lfsr.cpp
+++ lab01/lfsr.cpp
@@ -2,6 +2,9 @@
 #include "lfsr.h"
 
 void lfsr_calculate(uint16_t *reg) {
-    /* YOUR CODE HERE */
+    uint16_t bit = *reg & 1;
+    bit ^= *reg >> 2 & 1;
+    bit ^= *reg >> 3 & 1;
+    bit ^= *reg >> 5 & 1;
+    *reg = (*reg >> 1) | (bit << 15);
 }
-
--- lab01/ll_cycle.cpp
+++ lab01/ll_cycle.cpp
@@ -1,6 +1,14 @@
 #include "ll_cycle.h"
 
 bool ll_has_cycle(node *head) {
-    /* Ваш код должен быть написан только внутри этой функции */
-    return true;
+    node *turtle = head;
+    node *rabbit = head;
+
+    while (rabbit && rabbit->next)
+    {
+        rabbit = rabbit->next->next;
+        turtle = turtle->next;
+        if(rabbit == turtle) return true;
+    }
+    return false;
 }
