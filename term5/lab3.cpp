ad2fc22d07604b69e9799eab65caf4de9890760d
Andrushens <57043467+Andrushens@users.noreply.github.com>
2021-10-26 18:35:42 +0300

    Update lab3.cpp

--- term5/lab3.cpp
+++ term5/lab3.cpp
@@ -3,38 +3,45 @@
 #include <ctime>
 #include <stdio.h> 
 
-int main() 
+static void solve()
 {
     srand(time(0));
     const int size = 10000000;
-    int *A = new int[size];
-    int *B = new int[size];
+    int* A = new int[size];
+    int* B = new int[size];
     int start, i;
-    long long res = 0;
-
-    for (i = 0; i < size; i++) 
+    int res = 0;
+    for (i = 0; i < size; i++)
     {
         A[i] = rand() % 100 - rand() % 100;
-        B[i] = rand() % 100- rand() % 100;
+        B[i] = rand() % 100 - rand() % 100;
     }
-
     start = clock();
 
-    for (i = 0; i < size; i++) 
-    {
-        res += A[i] * B[i];
-    }
-
-    std::cout << "res: " << res << ", time: " << clock() - start << std::endl;
-    res = 0;
-    int chunk = size / 10;
-    start = clock();
-
-    #pragma omp parallel for default(shared) schedule(static,chunk) private(i) reduction(+:res)
     for (i = 0; i < size; i++)
     {
         res += A[i] * B[i];
     }
+    std::cout << "synch res: " << res << ", time: " << clock() - start << std::endl;
+    
+    res = 0;
+    int chunk = size / 8;
+    start = clock();
 
-    std::cout << "res: " << res << ", time: " << clock() - start << std::endl;
+    #pragma omp parallel for default(shared) schedule(static, chunk)\
+        private(i) reduction(+:res)
+    for (i = 0; i < size; i++)
+    {
+        res += A[i] * B[i];
+    }
+    std::cout << "paral res: " << res << ", time: " << clock() - start << std::endl << std::endl;
+}
+
+int main()
+{
+    solve();
+    solve();
+    solve();
+
+    return 0;
 }
